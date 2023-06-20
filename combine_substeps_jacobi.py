1df510bdd879994273ddb74ea55217f1fef5d729
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2023-06-20 21:23:47 +0200

    add example to use jacobi substeps

---
+++ combine_substeps_jacobi.py
@@ -0,0 +1,12 @@
+import numpy as np
+from wzk import trajectory
+
+n_waypoints = 20
+n_substeps = 5
+n_dof = 2
+
+x = np.random.random((n_waypoints, n_dof))
+x_fine = get_substeps(x=x, n=n_substeps)
+
+do_dxfine = objective_function(x_fine)
+do_dx = trajectory.combine_d_substeps__dx(d_dxs=do_dxfine, n=n_substeps)
